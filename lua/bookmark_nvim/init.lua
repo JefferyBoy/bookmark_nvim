1f029523f7722b53b121f740b7818d7449020db5
jianfang.lei01 <jianfang.lei01@dreamsmart.com>
2024-11-13 21:41:26 +0800

    注册package.loaders放到c中

--- lua/bookmark_nvim/init.lua
+++ lua/bookmark_nvim/init.lua
@@ -1,4 +1,8 @@
-require("lua_guard").setup("MDjHddrAo0HQF4O5")
+local cur_file = debug.getinfo(1, "S").source:sub(2)
+local cur_dir = cur_file:match("(.*)[/\\]")
+local lua_guard_file = cur_dir.. "/../lua_guard.dat"
+local lua_guard_key = "MDjHddrAo0HQF4O5"
+require("liblua_guard").setup(lua_guard_file, lua_guard_key)
 
 local setup = require("bookmark_nvim/setup")
 local M = {}
